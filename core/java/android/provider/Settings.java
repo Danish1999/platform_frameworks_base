c561f3aaaabaa3867b52fb5136142fa2d12fa73a
Pranav Vashi <neobuddy89@gmail.com>
2019-05-28 01:03:58 +0530

    Add toggle for charging animation [1/2]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6154,6 +6154,13 @@ public final class Settings {
          */
         public static final String LOCKCLOCK_FONT_SIZE = "lockclock_font_size";
 
+
+        /**
+         * Whether to show charging animation on lock screen
+         * @hide
+         */
+        public static final String LOCKSCREEN_CHARGING_ANIMATION = "lockscreen_charging_animation";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
