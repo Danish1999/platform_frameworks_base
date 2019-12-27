b4b425949df5797b06008d0dfbb2ffc7c432a0c1
Kshitij Gupta <kshitijgm@gmail.com>
2019-12-27 23:02:06 +0530

    base: QS themes [1/2]
    SDB: Adopt for Q
    
    base: Expose accent on QS Tiles
    
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>
    Change-Id: I492033f00adb4ecddde930b9b60ee051a00c2a02
    
    QS: Set Colors [1/2]
    
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>
    Change-Id: I1669e15729c6a78a5f0bc1f1bbbf70017db9376c
    
    QSContainerImpl: Toggle Dark QS Panel based on color
    
    - To avoid white on white or black on black, we can separate out the QS
      overlay and toggle it based on user selected color.
    
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>
    Change-Id: I2af220193e85e29ef33861de3858209a329e2927
    
    SystemUI: Allow theming QS with wallpaper colors [1/2]
    
    Also separate out wallpaper and user color values:
    - Move out the wallpaper colors to another setting and keep the users
      color value exclusive.
    - Set colors on QS based on the Wallpaper colors toggle.
    - This allows us to technically "restore" the old color when the
      wallpaper preference is toggled off.
    
    Change-Id: Ib3ec52cf0dcad2acf338e8198fb8be535b021769
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>
    
    StatusBar: Add missing observer for QS-Panel wallpaper-color setting
    
    - Missed this change implementing it.
    - Necessary for updating QS colors immediately.
    
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>
    Change-Id: I9761701d4edba5b370bee982e665dc2d34b6d174
    
    QS: Allow using frameworks resources exclusively [1/2]
    
    - We can load the framework drawable/color and apply that in order to
      restore the default QS color scheme.
    - Make it play well with our internal themer
    
    Change-Id: Ia705d4d6583bfc7c5b3e6b0e19fff12487000f5d
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>
    
    QSContainerImpl: Add assignment and null check for mColorExtractor
    
    Change-Id: Ic4d580b0520f7d94b521135ac9496c36cb7185e3
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>
    
    QSContainerImpl: Adapt for pie
    
    Change-Id: Ia1c90325f9371c72084a6244962634e60ee2072e
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>
    
    QSContainerImpl: Toggle QS Accent colors as necessary
    
    Change-Id: I735ff26a020f92bdc55d8004c37ccd4fb063db57
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>
    
    base: qs: Apply bg to active qs icons
    
    Change-Id: Ib22d5d490fb4856ab4cdf0a033c4373374529214
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>
    
    base: qs-colors: Minor fixups
    
    - Fixes an issue where qs tiles would be handled poorly on the default
      background. This is because the color extractor gives us null on
      default wallpaper when we try to get systemColors to update our system
      setting for QS to handle. Because of this, our QS never updates
      overlays and results in white-on-white.
    - Also fix an issue where the qs tile, when it's state is changed
      doesn't update icon color correctly. Fixed by allowing colors to be used
      in method animateGrayScale, which is designed to convert colors to gray
      scale and then animate change during toggle. This breaks our color
      leaving us with grey (and often even white/black) icons.
    
    base: QS: Allow coloring bg with accent [1/2]
    
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>
    Change-Id: I47b9b73cad0b8aab829ee39f0b7d76c74e2e2fb5

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -62,6 +62,7 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.StatusBarManager;
 import android.app.UiModeManager;
+import android.app.WallpaperColors;
 import android.app.WallpaperInfo;
 import android.app.WallpaperManager;
 import android.app.admin.DevicePolicyManager;
@@ -3752,6 +3753,7 @@ public class StatusBar extends SystemUI implements DemoMode,
             Dependency.get(ConfigurationController.class).notifyThemeChanged();
         }
         updateCorners();
+        updateQSPanel();
     }
 
     private void updateCorners() {
@@ -3766,6 +3768,27 @@ public class StatusBar extends SystemUI implements DemoMode,
             int resourceIdPadding = (int) mContext.getResources().getDimension(R.dimen.rounded_corner_content_padding);
             Settings.Secure.putIntForUser(mContext.getContentResolver(),
                 Settings.Secure.SYSUI_ROUNDED_CONTENT_PADDING, (int) (resourceIdPadding / density), UserHandle.USER_CURRENT);
+        }
+    }
+
+    private void updateQSPanel() {
+        int userQsWallColorSetting = Settings.System.getIntForUser(mContext.getContentResolver(),
+                    Settings.System.QS_PANEL_BG_USE_WALL, 0, UserHandle.USER_CURRENT);
+        boolean setQsFromWall = userQsWallColorSetting == 1;
+        if (setQsFromWall) {
+            WallpaperColors systemColors = mColorExtractor
+                    .getWallpaperColors(WallpaperManager.FLAG_SYSTEM);
+            if (systemColors != null)
+            {
+                Color mColor = systemColors.getPrimaryColor();
+                int mColorInt = mColor.toArgb();
+                Settings.System.putIntForUser(mContext.getContentResolver(),
+                        Settings.System.QS_PANEL_BG_COLOR_WALL, mColorInt, UserHandle.USER_CURRENT);
+            } else {
+                Settings.System.putIntForUser(mContext.getContentResolver(),
+                        Settings.System.QS_PANEL_BG_COLOR_WALL, Color.WHITE, UserHandle.USER_CURRENT);
+            }
+
         }
     }
 
@@ -4362,6 +4385,21 @@ public class StatusBar extends SystemUI implements DemoMode,
             resolver.registerContentObserver(Settings.Secure.getUriFor(
                     Settings.System.LOCKSCREEN_CHARGING_ANIMATION),
                     false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+	            Settings.System.QS_PANEL_BG_USE_WALL),
+                    false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.QS_PANEL_BG_USE_FW),
+                    false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                     Settings.System.QS_PANEL_BG_COLOR),
+                    false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.QS_PANEL_BG_COLOR_WALL),
+                    false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+	            Settings.System.QS_PANEL_BG_USE_ACCENT),
+	            false, this, UserHandle.USER_ALL);
         }
 
          @Override
@@ -4381,6 +4419,15 @@ public class StatusBar extends SystemUI implements DemoMode,
             } else if (uri.equals(Settings.System.getUriFor(
                     Settings.System.LOCKSCREEN_CHARGING_ANIMATION))) {
                 updateChargingAnimation();
+            } else if (uri.equals(Settings.System.getUriFor(
+                    Settings.System.QS_PANEL_BG_USE_WALL))) {
+                updateQSPanel();
+                mQSPanel.getHost().reloadAllTiles();
+            } else if (uri.equals(Settings.System.getUriFor(Settings.System.QS_PANEL_BG_USE_FW)) ||
+                    uri.equals(Settings.System.getUriFor(Settings.System.QS_PANEL_BG_COLOR)) ||
+                    uri.equals(Settings.System.getUriFor(Settings.System.QS_PANEL_BG_COLOR_WALL)) ||
+                    uri.equals(Settings.System.getUriFor(Settings.System.QS_PANEL_BG_USE_ACCENT))) {
+                mQSPanel.getHost().reloadAllTiles();
             }
             update();
         }
@@ -4402,6 +4449,7 @@ public class StatusBar extends SystemUI implements DemoMode,
             updateNavigationBar(false);
             updateKeyguardStatusSettings();
             updateChargingAnimation();
+            updateQSPanel();
        }
     }
 
